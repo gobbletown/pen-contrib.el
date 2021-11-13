e46d1a58f83bb00d8d8bfbf55e594df11b35a66a
Shane Mulligan <mullikine@gmail.com>
2021-11-13 21:56:37 +1300

    Sat 13 Nov 2021 21:56:37

--- src/pen-org-brain.el
+++ src/pen-org-brain.el
@@ -720,16 +720,15 @@ suggest-full-list will ask if you want to add the entire list as subtopics to th
   (let ((topic (org-brain-current-topic)))
 
     (let ((answer
-           (eval
-            `(pen-ci
-              (pen-snc
-               "pen-pretty-paragraph"
-               (pen-single-batch
-                (pf-generic-tutor-for-any-topic/2
-                 ,topic
-                 ;; ,cname
-                 ;; ,pname
-                 ,question)))))))
+           (pen-snc
+            "pen-pretty-paragraph"
+            (eval
+             `(pen-single-batch
+               (pf-generic-tutor-for-any-topic/2
+                ,topic
+                ;; ,cname
+                ;; ,pname
+                ,question))))))
       (if (interactive-p)
           (pen-etv answer)
         answer))))
